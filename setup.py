c075a70f125c17726ca331094476df9ac747d59d
limuloo <zdw1999@zju.edu.cn>
2025-01-09 22:20:23 -0500

    upd

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="3DIS",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author="Dewei Zhou",
+    author_email="zdw1999@zju.edu.cn",
+    description="official implementation of the paper 3DIS: DEPTH-DRIVEN DECOUPLED INSTANCE SYNTHESIS FOR TEXT-TO-IMAGE GENERATION",
+    url="https://github.com/limuloo/3DIS",
+)
